d8d9c6af17b38d6ec8f897152d578ff443ba8194
Kenyaholland <kenyamholland@gmail.com>
2026-04-26 21:36:30 -0600

    removed unused terraform output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -4,8 +4,3 @@ output "table_name" {
   value = aws_dynamodb_table.exam_items.name
   description = "Exposed for CI/CD pipelines and integration testing"
 }
-
-output "api_url" {
-  value = aws_api_gateway_stage.prod.invoke_url
-  description = "Exposed for integration testing"
-}
